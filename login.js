7f251c2fa9e26a41f988b3f3b7cccf8282324406
clayhendrickson64-del <clayhendrickson64@gmail.com>
2026-07-20 12:01:00 -0500

    Create login.js

---
+++ login.js
@@ -0,0 +1,7 @@
+const gameText=document.getElementById('game-text2');
+const userInput=document.getElementById('user-input2');
+const submitBtn=document.getElementById('submit-btn2');
+funchion print(text){
+  game Text.inner.Text+=text+"/n";
+  gameText.scrollTop=gameText.scrollHeight;
+}
